e5104cd8517858a720dc00f7c1210ddcf38e49fa
LinJXuan <234662656@qq.com>
2020-06-04 20:54:00 +0800

    关卡跳转

---
+++ demo/Assets/Statistic.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Statistic : MonoBehaviour
+{
+    private int point = 0;
+    // Start is called before the first frame update
+    private static Statistic instance = new Statistic();
+    private Statistic() { }
+    public static Statistic getInstance()
+    {
+        return instance;
+    }
+    public void setPoint(int point)
+    {
+        this.point += point;
+    }
+
+    public int getPoint()
+    {
+        return this.point;
+    }
+}
