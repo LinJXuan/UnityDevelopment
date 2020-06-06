587d9bde279e8d0aee5755d98c9950a24174a265
CCYang <478022020@qq.com>
2020-06-06 12:54:31 +0800

    加入背包、属性场景

---
+++ demo/Assets/scripts/Plus2.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+ 
+public class Plus2 : MonoBehaviour {
+    public static float x = 0;
+    public void hit_test() {
+        x += 1;
+        string str = x.ToString();
+        GameObject.Find("Canvas/Number2").GetComponent<Text>().text = str;
+    }
+}
