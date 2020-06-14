9aeea4ee05f4dc17cdb5c3641b26266913a05aa9
CCCYounger <478022020@qq.com>
2020-06-14 13:15:01 +0800

    背包界面与战斗界面绑定

---
+++ demo/Assets/scripts/ShowBag.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ShowBag : MonoBehaviour {
+    // Start is called before the first frame update
+    public static GameObject object1;
+    public static GameObject object2;
+    void Start () {
+        object1 = GameObject.Find ("Canvas");
+        object2 = GameObject.Find ("Bag/Canvas");
+        object1.SetActive (true);
+        object2.SetActive (false);
+    }
+
+    // Update is called once per frame
+    void Update () {
+
+    }
+    public void open () {
+        object1.SetActive (false);
+        object2.SetActive (true);
+
+    }
+    public void close () {
+        object1.SetActive (true);
+        object2.SetActive (false);
+    }
+}
