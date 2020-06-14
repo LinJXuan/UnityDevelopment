d285dc2c14bb30403b606eebf7b74ca86d8ea06a
CCCYounger <478022020@qq.com>
2020-06-14 14:02:31 +0800

    暂停界面与战斗界面绑定

--- demo/Assets/scripts/ShowBag.cs
+++ demo/Assets/scripts/ShowBag.cs
@@ -7,11 +7,14 @@ public class ShowBag : MonoBehaviour {
     // Start is called before the first frame update
     public static GameObject object1;
     public static GameObject object2;
+    public static GameObject object3;
     void Start () {
         object1 = GameObject.Find ("Canvas");
         object2 = GameObject.Find ("Bag/Canvas");
+        object3 = GameObject.Find ("Pause/Canvas");
         object1.SetActive (true);
         object2.SetActive (false);
+        object3.SetActive (false);
     }
 
     // Update is called once per frame
@@ -21,10 +24,18 @@ public class ShowBag : MonoBehaviour {
     public void open () {
         object1.SetActive (false);
         object2.SetActive (true);
+        object3.SetActive (false);
 
+    }
+     public void pause(){
+        object1.SetActive (false);
+        object2.SetActive (false);
+        object3.SetActive (true);
     }
     public void close () {
         object1.SetActive (true);
         object2.SetActive (false);
+        object3.SetActive (false);
     }
+   
 }
