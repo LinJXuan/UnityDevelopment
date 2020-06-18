825d87612b8db5c1753fa6b6aa22364c3f2c96ae
lshuang_se <lshuang.se@foxmail.com>
2020-06-18 19:47:27 +0800

    完成突刺、圣光、投掷的技能伤害实现，技能动画待实现

--- demo/Assets/scripts/enemyController.cs
+++ demo/Assets/scripts/enemyController.cs
@@ -1,11 +1,12 @@
-﻿using System;
+﻿using DG.Tweening;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 public class EnemyController : MonoBehaviour {
     //属性
-    public GameObject player;
+    private GameObject player;
     public float speed;
     public float attackRange;
     public float timeAttack = 5f;
@@ -26,6 +27,11 @@ public class EnemyController : MonoBehaviour {
 
     private bool look = false;
     private Vector3 position;
+
+    //飘血
+    public static GameObject flaptext;
+    public Text flapWord;
+
     void Start () {
         s = Statistic.getInstance ();
         rbody = GetComponent<Rigidbody> ();
@@ -44,29 +50,37 @@ public class EnemyController : MonoBehaviour {
                 attackRange = expert.getRange ();
                 attack = expert.getAttack ();
                 Hp = expert.getHp ();
-                speed = normal.getSpeed ();
+                speed = expert.getSpeed ();
                 break;
             case 'B':
                 boss = BossEnemy.getInstance ();
                 attackRange = boss.getRange ();
                 attack = boss.getAttack ();
                 Hp = boss.getHp ();
-                speed = normal.getSpeed ();
+                speed = boss.getSpeed ();
                 break;
         }
         enemyBlood.value = Hp;
         enemyBlood.maxValue = Hp;
         transform.LookAt (player.transform);
         position = transform.position;
+
+        //飘血
+        flaptext = GameObject.Find("flapWord");
+        flaptext.SetActive(false);
     }
 
     // Update is called once per frame
     void Update () {
+        //print(rbody.position.x);
         Vector3 worldPos = new Vector3 (transform.position.x, transform.position.y + 3f, transform.position.z);
         Vector3 screenPos = Camera.main.WorldToScreenPoint (worldPos);
         //血条位置
         enemyBlood.transform.position = new Vector3 (screenPos.x, screenPos.y, screenPos.z);
         enemyBlood.value = Hp;
+        //伤害飘字的位置
+        //有问题
+        flapWord.transform.localPosition = new Vector3(transform.localPosition.x, transform.localPosition.y + 50f, transform.localPosition.z);
     }
 
     private void FixedUpdate () {
@@ -94,15 +108,20 @@ public class EnemyController : MonoBehaviour {
         }
 
         Hp -= damage;
+        if (flaptext != null)
+        {
+            flaptext.SetActive(true);     //显示伤害
+            FlyTo(flapWord);
+        }
+
         if (transform.name[0] == 'B') {
             boss.setcurrentHp (Hp);
         }
 
         if (Hp <= 0) {
-            Death ();
+            Death();
             return;
         }
-        print ("enemyHp ===>" + Hp);
     }
 
     public void AddlevelPoint () { //掉落升级点数
@@ -129,10 +148,29 @@ public class EnemyController : MonoBehaviour {
     private void Death () {
         isDead = true;
         AddlevelPoint();//掉落升级点数
-        PlusAttribute.setAll();//写入属性
+        //PlusAttribute.setAll();//写入属性
         //设置获得积分
         s.setPoint (10);
         Destroy (gameObject);
+        print("enemy destory");
     }
 
+    //伤害飘字函数
+    public static void FlyTo(Graphic graphic)
+    {
+        RectTransform rt = graphic.rectTransform;
+        Color c = graphic.color;
+        c.a = 0;
+        graphic.color = c;
+        Sequence mySequence = DOTween.Sequence();
+        Tweener move1 = rt.DOMoveY(rt.position.y + 50, 0.5f);
+        Tweener move2 = rt.DOMoveY(rt.position.y + 100, 0.5f);
+        Tweener alpha1 = graphic.DOColor(new Color(c.r, c.g, c.b, 1), 0.5f);
+        Tweener alpha2 = graphic.DOColor(new Color(c.r, c.g, c.b, 0), 0.5f);
+        mySequence.Append(move1);
+        mySequence.Join(alpha1);
+        // mySequence.AppendInterval(1);
+        mySequence.Append(move2);
+        mySequence.Join(alpha2);
+    }
 }
